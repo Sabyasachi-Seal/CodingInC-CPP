a146ab51377c879fb4911baf5a35c2c8aa1d95ff
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-03-29 18:37:10 +0530

    Done.

--- 4th Sem/Others/fileinput.c
+++ 4th Sem/Others/fileinput.c
@@ -3,11 +3,11 @@ int main(){
     int n;
     FILE *file = fopen("inputs.txt", "r");
     fscanf(file, "%d", &n);
-    int arr[n][n];
+    int arr[n];
     for(int i=0; i<n; i++){
         fscanf(file, "%d", &arr[i]);
     }
-    for(int i=1; i<n; i++){
+    for(int i=0; i<n; i++){
         printf("%d ", arr[i]);
     }
 }
