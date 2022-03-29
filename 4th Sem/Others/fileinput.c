a08a6658af299bb35691fe75c5acc20db71c3a25
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-03-29 18:32:45 +0530

    Done

--- 4th Sem/Others/fileinput.c
+++ 4th Sem/Others/fileinput.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 int main(){
     int n;
-    printf("Enter number of inputs: ");
-    scanf("%d", &n);
-    int arr[n][n];
     FILE *file = fopen("inputs.txt", "r");
+    fscanf(file, "%d", &n);
+    int arr[n][n];
     for(int i=0; i<n; i++){
-        for(int j=0; j<5; j++){
-            fscanf(file, "%d", &arr[i][j]);
-        }
+        fscanf(file, "%d", &arr[i]);
     }
-    for(int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
-            printf("%d ", arr[i][j]);
-        }
-        printf("\n");
+    for(int i=1; i<n; i++){
+        printf("%d ", arr[i]);
     }
-    return 0;
 }
