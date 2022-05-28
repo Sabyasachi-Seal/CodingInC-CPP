9eab6995d200c5836132358c7780326442f7397e
Sabyasachi Seal <yoboy907@gmail.com>
2022-05-28 20:40:24 +0530

    Max Sub Array

---
+++ 4th Sem/maxarraysum.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// kadens algorithm
+int main(){
+    int arr[] = {4, 3, -1 , 105, 45, -100, 23, 64, -1};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int i;
+    int sum = arr[0];
+    int max = sum;
+    int start = 0, end=0;
+    for(i=1; i<size; i++){
+        if(sum<0){
+            start = i;
+            sum = 0;
+        }
+        else{
+           sum += arr[i];
+        }
+        if(sum>max){
+            end = i;
+            max = sum;
+        }
+    }
+    for(i=start; i<=end; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n%d\n", max);
+}
